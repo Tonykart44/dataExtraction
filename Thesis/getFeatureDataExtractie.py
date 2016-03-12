9a375975ba72d54c99566373e1930d1dd6b981d6
Robin Amsters <robinamsters@gmail.com>
2016-03-12 22:13:57 +0100

    Moved functions
    Defined function at top of script

--- Thesis/getFeatureDataExtractie.py
+++ Thesis/getFeatureDataExtractie.py
@@ -6,13 +6,36 @@ Created on Wed Mar  9 12:00:30 2016
 
 File that extraxts data from txt file that contains getFeature data from txt files
 """
+"""
+DEFINING FUNCTIONS
+"""
 
+def checkReference(reference, data, accuracy):
+    """
+    A function to check whether a dataset matches the reference close enough
+    
+        INPUTS:
+                reference: reference to match data against, this is a list
+                           containing an arbitrary amount of lists of size 2
+                data: data to be matched with the reference, 
+                      this is a list of size 2
+                accuracy: parameter that specifies how much data can differ 
+                          from reference and still be accepted
+    
+        OUTPUTS
+                matchedData: ???
+    """
+    
+    for ref in reference:
+        print(ref)
+        for dat in data:
+            print(dat)
 
 # Opening file and reading contents
 filePath = '/home/robin/Bureaublad/getFeatureCalibratieData.txt'
 fileHandle = open(filePath, 'r')
 lines = fileHandle.readlines()
-lines = lines[0:]
+lines = lines[0:30]
 
 # Defining variables
 startMeasurement = False # When true, next lines can be considered to be part of the same measurement
@@ -42,28 +65,10 @@ for line in lines:
     if newLineCount >= 2 and startMeasurement:
         # Measurements are separated by two newlines
         cornerMatrix.append(currentMeasurement)
+        checkReference(refMeasurements, currentMeasurement, 5)
         currentMeasurement = []
         startMeasurement = False
         newLineCount = 0
 
-"""
-DEFINING FUNCTIONS
-"""
 
-def checkReference(reference, data, accuracy):
-    """
-    A function to check wheter a dataset matches the reference close enough
-    
-        INPUTS:
-                reference: reference to match data against, this is a list
-                           containing an arbitrary amount of lists of size 2
-                data: data to be matched with the reference, 
-                      this is a list of size 2
-                accuracy: parameter that specifies how much data can differ 
-                          from reference and still be accepted
-    
-        OUTPUTS
-                matchedData: ???
-    """
-    
-    pass    
+        
