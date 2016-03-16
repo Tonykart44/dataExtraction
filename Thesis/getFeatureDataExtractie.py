0a1ab8b4ce41ed6802a3a07895cf03b4a02d4977
Robin Amsters <robinamsters@gmail.com>
2016-03-16 09:25:06 +0100

    checkReference move
    checkReference() is different for each case, thus it was moved from the library to the scrips

--- Thesis/getFeatureDataExtractie.py
+++ Thesis/getFeatureDataExtractie.py
@@ -10,16 +10,81 @@ import dataExtraction as dE
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
+import math
 
 """
 DEFINING FUNCTIONS
 """   
+#==============================================================================
+def checkReference(reference, data, accuracy):
+    """
+    Function to check whether a dataset matches the reference close enough
+    
+        INPUT:
+                reference: reference to match data against, this is a list
+                           containing an arbitrary amount of lists of size 1 or 
+                           2
+                data: data to be matched with the reference, 
+                      this is a list containing lists containing lists of len 1
+                      or 2
+                accuracy: parameter that specifies how much data can differ 
+                          from reference and still be accepted as matching
+    
+        OUTPUT
+                matchedData: a list containing lists of size 1 or 2 of data 
+                             that has been checked against the reference and 
+                             accepted according to the specified accuracy. 
+                             Number inside list represents difference of 
+                             datapoint with reference
+                             
+    """
+    matchedData = [] #initalizing output
+    lengthError = '''ERROR: length of input is incorrect, please use only 
+    lists with sublists that have a length of 1 or 2.'''
+    
+    for measurement in data:
+        matchedPoint = [] #datapoints (list of len 2) that have been matched
+        
+        for point in measurement: #Checking and selecting measurements
+        
+            if not len(point) == 2:
+                print lengthError
+                
+            else:
+                x_p = point[0] #measured x coordinates
+                y_p = point[1] #measured y coordinates
+
+            for ref in reference:
+
+                if not len(ref) == 2:
+                    print lengthError
+                    
+                else:
+                    x_ref = ref[0] #reference x coordinates
+                    y_ref = ref[1] #reference u coordinates
+                    
+                    diff_x = float(x_p) - float(x_ref) #difference in x coordinates
+                    diff_y = float(y_p) - float(y_ref) #difference in y coordinates
+                    
+                    pointDiff = [diff_x, diff_y] #difference as list
+                    distance = math.sqrt(math.pow(diff_x, 2) + math.pow(diff_y, 2))
+                    
+                    if distance <= accuracy and point not in matchedPoint:
+                        # Add current point to matched points if accepted and not already in matchedPoint
+                        matchedPoint.append(pointDiff)
+
+                    if len(matchedPoint) == len(reference) and matchedPoint not in matchedData:
+                        # Add points to data when all have been matched against a reference and they are not yet in matchedData
+                        matchedData.append(matchedPoint)
+    
+    return matchedData
+
 def getDistribution(filePath, refMeasurements):
     #List containing all corner measurements
     measurements = dE.getMeasurements(filePath,'corners_world =') 
     
     # Measurements which are not due to random noise
-    filteredMeasurements = dE.checkReference(refMeasurements, measurements, 0.2) 
+    filteredMeasurements = checkReference(refMeasurements, measurements, 0.2) 
     
     #Split filtered corners in list of points that belong together
     measurements_point_A = dE.removeSublistLevel(filteredMeasurements,0)
--- Thesis/getOdometryDataExtractie.py
+++ Thesis/getOdometryDataExtractie.py
@@ -19,15 +19,75 @@ from scipy.stats import norm
 """
 DEFINING FUNCTIONS
 """
+def checkReference(reference, data, accuracy):
+    """
+    Function to check whether a dataset matches the reference close enough
+    
+        INPUT:
+                reference: reference to match data against, this is a list
+                           containing an arbitrary amount of lists of size 1 or 
+                           2
+                data: data to be matched with the reference, 
+                      this is a list containing lists containing lists of len 1
+                      or 2
+                accuracy: parameter that specifies how much data can differ 
+                          from reference and still be accepted as matching
+    
+        OUTPUT
+                matchedData: a list containing lists of size 1 or 2 of data 
+                             that has been checked against the reference and 
+                             accepted according to the specified accuracy. 
+                             Number inside list represents difference of 
+                             datapoint with reference
+                             
+    """
+    matchedData = [] #initalizing output
+    lengthError = '''ERROR: length of input is incorrect, please use only 
+    lists with sublists that have a length of 1 or 2.'''
+    
+    index = 0
+    
+    for measurement in data:
+#        matchedPoint = [] #datapoints (list of len 2) that have been matched
+        
+        for point in measurement: #Checking and selecting measurements
+        
+            if not len(point) == 1:
+                print lengthError
+                
+            else:
+                data_point = point[0]
+                
+                ref = reference[index]
+
+                if not len(ref) == 1:
+                    print lengthError
+                    
+                else:
+                    ref_point = ref[0]
+                    
+                    distance = abs(float(data_point)-float(ref_point))
+                
+                    matchedData.append(distance)                    
+                    
+#                    if distance <= accuracy and point not in matchedPoint:
+#                        # Add current point to matched points if accepted and not already in matchedPoint
+#                        matchedPoint.append(distance)
+#
+#                    if len(matchedPoint) == len(reference) and matchedPoint not in matchedData:
+#                        # Add points to data when all have been matched against a reference and they are not yet in matchedData
+#                        matchedData.append(matchedPoint)
+            index += 1
+    return matchedData
 
 def getDistribution(filePath, refMeasurements_ds, refMeasurements_dth):
     #List containing all corner measurements
     measurements_ds = dE.getMeasurements(filePath,'ds =')
-    measurements_dth = dE.getMeasurements(filePath,'d =')
+    measurements_dth = dE.getMeasurements(filePath,'dth =')
     
     # Measurements which are not due to random noise
-    filteredMeasurements_ds = dE.checkReference(refMeasurements_ds, measurements_ds, 0.2) 
-    filteredMeasurements_dth = dE.checkReference(refMeasurements_dth, measurements_dth, 0.2)  
+    filteredMeasurements_ds = checkReference(refMeasurements_ds, measurements_ds, 0.2) 
+    filteredMeasurements_dth = checkReference(refMeasurements_dth, measurements_dth, 0.2)  
 #    #Split filtered corners in list of points that belong together
 #    measurements_point_A = dE.removeSublistLevel(filteredMeasurements_ds,0)
 #    measurements_point_B = dE.removeSublistLevel(filteredMeasurements_dth,1)
@@ -49,6 +109,7 @@ def getDistribution(filePath, refMeasurements_ds, refMeasurements_dth):
     
 #    return mu, std, allDifferences
     return filteredMeasurements_ds, filteredMeasurements_dth
+    
 """ 
 MAIN SCRIPT: USING FUNCTIONS TO EXTRACT DATA
 """
@@ -57,6 +118,8 @@ MAIN SCRIPT: USING FUNCTIONS TO EXTRACT DATA
 filePath = '/home/robin/Bureaublad/getOdometrySampleData.txt'
 refMeasurements_ds = [[0], [0.0255], [0.0209]] # reference data which is considered correct (from camera)
 refMeasurements_dth = [[0], [-0.004], [-0.0080]]
+measurements_ds = dE.getMeasurements(filePath,'ds =')
+measurements_dth = dE.getMeasurements(filePath,'dth =')
 filteredMeasurements_ds, filteredMeasurements_dth = getDistribution(filePath, refMeasurements_ds, refMeasurements_dth)
 
 
