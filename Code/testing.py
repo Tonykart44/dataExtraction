5f87fbd5961710525a362287702beb13560ecfb2
Robin Amsters <robinamsters@gmail.com>
2016-03-21 13:38:15 +0100

    Add testfile

---
+++ Code/testing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 13:35:54 2016
+
+@author: Robin Amsters
+
+test file for dataAnalysis
+"""
+
+from dataAnalysis import GetFeature
+
+testRef = []
+
+testGetFeature = GetFeature(testRef)
